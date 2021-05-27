68b0b546d28817d2ffcb54dce61a51926b1e8d63
Setsu <cryptiareta@gmail.com>
2021-05-27 08:44:40 +0200

    adding real content right there

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -3,7 +3,7 @@ const navAnimation = () =>{
   const navigation = document.getElementById("navigation");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 30) {
+    if (window.scrollY > 100) {
       navigation.classList.add("anim-nav");
     } else {
       navigation.classList.remove("anim-nav");
